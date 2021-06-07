19f3f071b29d4b383d9a13d413fbb56776821993
mengxyz <meng348@gmail.com>
2021-06-08 02:52:26 +0700

    [changed] change bottomAppBar Color to surface color

--- app/src/main/java/com/github/psm/movie/review/MainActivity.kt
+++ app/src/main/java/com/github/psm/movie/review/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -41,7 +38,7 @@ fun MovieAppBody() {
     Scaffold(
         bottomBar = {
             BottomNavigation(
-                backgroundColor = Color.White
+                backgroundColor = MaterialTheme.colors.surface
             ) {
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentRoute = navBackStackEntry?.destination?.route
