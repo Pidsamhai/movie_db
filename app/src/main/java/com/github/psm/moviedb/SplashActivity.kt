3bf91797609f34bd3a80b91c1d2c217dc5a60244
mengxyz <meng348@gmail.com>
2021-08-17 03:19:52 +0700

    [changed] update new splash screen

--- app/src/main/java/com/github/psm/moviedb/SplashActivity.kt
+++ app/src/main/java/com/github/psm/moviedb/SplashActivity.kt
@@ -2,38 +2,36 @@ package com.github.psm.moviedb
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.psm.moviedb.ui.theme.MovieDbTheme
+import kotlinx.coroutines.delay
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieDbTheme {
-                SplashContent()
+                SplashContent(
+                    end = {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                )
             }
         }
     }
@@ -41,54 +39,54 @@ class SplashActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun SplashContent() {
-    val activity = (LocalContext.current as ComponentActivity)
-    val infiniteTransition = rememberInfiniteTransition()
-    val infiniteAnimateFloat = infiniteTransition.animateFloat(
-        initialValue = 1f,
-        targetValue = 0.1f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(800, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-    LaunchedEffect(key1 = null) {
-        Handler(Looper.myLooper()!!).postDelayed({
-            activity.startActivity(Intent(activity, MainActivity::class.java))
-            activity.finishAffinity()
-        }, 2500)
-    }
-    Scaffold {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(1f),
-                verticalArrangement = Arrangement.Center
-            ) {
-                Image(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .clip(RoundedCornerShape(16.dp))
-                        .alpha(infiniteAnimateFloat.value)
-                        .background(Color(0XFF86CECB)),
-                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                    contentDescription = null
-                )
+fun SplashContent(
+    end: () -> Unit = { }
+) {
+    val minScale = 0.5f
+    val defaultScale = 1f
+    val maxScale = 50f
+    var scale by remember { mutableStateOf(defaultScale) }
+    val animScale by animateFloatAsState(
+        targetValue = scale,
+        animationSpec = tween(
+            durationMillis = 800,
+            easing = FastOutSlowInEasing
+        ),
+        finishedListener = {
+            when (it) {
+                maxScale -> end()
+                minScale -> scale = maxScale
             }
-            Text(
-                modifier = Modifier
-                    .padding(16.dp),
-                text = "Versions ${BuildConfig.VERSION_NAME}",
-                style = MaterialTheme
-                    .typography
-                    .caption
-                    .copy(
-                        fontWeight = FontWeight.Bold
-                    )
-            )
         }
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        delay(800)
+        scale = minScale
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        contentAlignment = Alignment.Center
+    ) {
+
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .size(size = 200.dp)
+                .scale(animScale),
+            painter = painterResource(R.drawable.ic_app_logo),
+            contentDescription = null
+        )
     }
 }
